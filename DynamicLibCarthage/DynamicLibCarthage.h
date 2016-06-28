40ea6dabdb6096bb4c264252a553fabd39dbb828
jack930820 <quanmai@clwtekiMacBook-Pro.local>
2016-06-28 15:53:50 +0800

    first
    first

---
+++ DynamicLibCarthage/DynamicLibCarthage.h
@@ -0,0 +1,19 @@
+//
+//  DynamicLibCarthage.h
+//  DynamicLibCarthage
+//
+//  Created by quanmai on 16/6/28.
+//  Copyright © 2016年 quanmai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicLibCarthage.
+FOUNDATION_EXPORT double DynamicLibCarthageVersionNumber;
+
+//! Project version string for DynamicLibCarthage.
+FOUNDATION_EXPORT const unsigned char DynamicLibCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicLibCarthage/PublicHeader.h>
+
+
